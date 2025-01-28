af80f2023157beebdc7cd49a74362c6ae3bd52da
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2025-01-29 01:07:02 +0800

    trip not found page

--- src/app/trip/[id]/layout.tsx
+++ src/app/trip/[id]/layout.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/header";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import ButtonLink from "@/components/ui/button-link";
 import { auth } from "@/server/auth";
 import { db } from "@/server/db";
 import { location, trip, tripDay } from "@/server/db/schema";
@@ -28,6 +30,42 @@ export default async function TripLayout({
       .innerJoin(location, eq(location.id, trip.locationId))
       .where(eq(trip.id, id)),
   ]);
+
+  if (data.length === 0)
+    return (
+      <div className="flex min-h-dvh flex-col">
+        <Header>
+          <Avatar>
+            <AvatarImage
+              src={
+                userSession && userSession.user.image
+                  ? userSession.user.image
+                  : undefined
+              }
+            />
+            <AvatarFallback>
+              {userSession
+                ? userSession.user.name.substring(0, 2).toUpperCase()
+                : "NA"}
+            </AvatarFallback>
+          </Avatar>
+        </Header>
+        <main className="mx-auto flex w-full max-w-screen-sm grow flex-col items-center justify-center gap-12 p-4">
+          <div className="text-center">
+            <h1 className="mb-3 font-display text-4xl font-semibold">
+              Trip Not Found!
+            </h1>
+            <h3 className="text-lg text-slate-700">
+              We can&apos;t seem to find the trip that you&apos;re looking for.
+              The trip id may be incorrect or the trip may have been deleted.
+            </h3>
+          </div>
+          <ButtonLink href="/" size="large">
+            Back to Home
+          </ButtonLink>
+        </main>
+      </div>
+    );
 
   return (
     <TripProviders data={data} session={userSession}>
