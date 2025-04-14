266802ecd48031cdb9bb3009e552d6223dbfad7d
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2025-04-14 23:36:18 +0800

    404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+import ButtonLink from "@/components/ui/button-link";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Loading() {
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-gray-50 p-4">
+      <Link
+        href="/"
+        className="flex items-baseline gap-1 rounded-full p-1.5 ring-offset-white transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2"
+      >
+        <Image src="/outbound.svg" width={20} height={20} alt="outbound" />
+        <h1 className="hidden font-display text-3xl font-semibold leading-5 text-brand-900 sm:inline">
+          Outbound
+        </h1>
+      </Link>
+      <h1 className="mt-4 text-center font-display text-5xl font-semibold text-slate-900 sm:text-7xl">
+        Page not found
+      </h1>
+      <p className="text-center text-lg font-medium text-gray-500 sm:text-xl/8">
+        Sorry, we couldn&apos;t find the page you&apos;re looking for.
+      </p>
+      <div className="mt-10 flex items-center justify-center gap-x-6">
+        <ButtonLink href="/" size="large">
+          Go Back Home
+        </ButtonLink>
+      </div>
+    </main>
+  );
+}
